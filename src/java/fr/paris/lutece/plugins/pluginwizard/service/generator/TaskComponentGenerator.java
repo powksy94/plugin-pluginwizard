258ce9b7ee60c2f08f432ebc87eaf6da99a8d30d
ClaraFlo2 <clara.florac@paris.fr>
2025-06-10 17:36:50 +0200

    update copyright

--- src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/TaskComponentGenerator.java
+++ src/java/fr/paris/lutece/plugins/pluginwizard/service/generator/TaskComponentGenerator.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2002-2025, City of 
-
+ * Copyright (c) 2002-2025, City of Paris
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
